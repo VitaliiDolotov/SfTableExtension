75ca6e74110942a213b5a5d6cd0851b5506e2e1e
Vitalii Dolotov <vitaliy.dolotov@gmail.com>
2021-11-26 19:47:20 +0200

    Removed not used namespaces

--- SfTableExtension/Test.cs
+++ SfTableExtension/Test.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using FluentAssertions;
 using TechTalk.SpecFlow;
-using TechTalk.SpecFlow.Assist;
-using TechTalk.SpecRun.Common.Helper;
 using Xunit;
 
 namespace SfTableExtension

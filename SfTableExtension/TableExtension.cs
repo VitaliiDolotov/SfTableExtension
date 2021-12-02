d96af870195933bf3d53631229a7b683e8a92965
Anastasiia Liashova <anastasiia.liashova@tech-stack.io>
2021-12-02 13:13:31 +0200

    Added MemberInfo

--- SfTableExtension/TableExtension.cs
+++ SfTableExtension/TableExtension.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
+using System.Reflection;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
 using TechTalk.SpecRun.Common.Helper;
@@ -27,6 +27,11 @@ namespace SfTableExtension
             var allVariablesNames = properties.Select(x => x.Name)
                 .Concat(fields.Select(x => x.Name).ToList());
 
+            var listMember = new List<MemberInfo>();
+            listMember.AddRange(fieldsList);
+            listMember.AddRange(propertiesList);
+
+
             foreach (var row in table.Rows.Skip(1))
             {
                 if (row.Any(cell => allVariablesNames.Contains(cell.Key)
@@ -36,32 +41,34 @@ namespace SfTableExtension
                     continue;
                 }
 
-                foreach (var property in properties)
+                foreach (var member in listMember)
                 {
-                    if (!row.ContainsKey(property.Name)) continue;
-                    if (!IsCollectionType<IEnumerable>(property.PropertyType)) continue;
-                    if (row[property.Name].IsNullOrEmpty()) continue;
+                    if (!row.ContainsKey(member.Name)) continue;
+                    if (!IsCollectionType<IEnumerable>(GetType(member))) continue;
+                    if (row[member.Name].IsNullOrEmpty()) continue;
 
-                    object propertyValue = row[property.Name];
+                    object propertyValue = row[member.Name];
 
-                    var propertyType = IsCollectionType<Array>(property.PropertyType) ?
-                        property.PropertyType.GetElementType() :
-                        property.PropertyType.GenericTypeArguments.First();
+                    var propertyType = IsCollectionType<Array>(GetType(member)) ?
+                        GetType(member).GetElementType() :
+                        GetType(member).GenericTypeArguments.First();
 
                     if (propertyType is null)
-                        throw new Exception($"Unable to get {property.Name} property type");
+                        throw new Exception($"Unable to get {member.Name} property type");
 
                     propertyValue = Parse(propertyType, propertyValue);
 
-                    var getInstanceValue = property.GetValue(instances.Last());
+                    var GetValueMethod = GetMethod(member, ValueMethods.GetValue);
+                    var getInstanceValue = GetValueMethod.Invoke(member, new object?[] { instances.Last() });
                     // Add value to array Type property
-                    if (IsCollectionType<Array>(property.PropertyType))
+                    if (IsCollectionType<Array>(GetType(member)))
                     {
                         var resultList = ((IEnumerable)getInstanceValue).Cast<object>().ToList();
                         resultList.Add(propertyValue);
                         var resultArray = Array.CreateInstance(propertyType, resultList.Count);
                         Array.Copy(resultList.ToArray(), resultArray, resultList.Count);
-                        property.SetValue(instances.Last(), resultArray);
+                        var SetValueMethod = GetMethod(member, ValueMethods.SetValue);
+                        SetValueMethod.Invoke(member, new object?[] { instances.Last(), resultArray });
                     }
                     // Add value to Collection type property
                     else
@@ -69,49 +76,42 @@ namespace SfTableExtension
                         var iCollectionObject = typeof(ICollection<>).MakeGenericType(propertyType);
                         var addMethod = iCollectionObject.GetMethod("Add");
                         addMethod.Invoke(getInstanceValue, new object[] { propertyValue });
-                        property.SetValue(instances.Last(), getInstanceValue);
-                    }
-                }
 
-                foreach (var field in fieldsList)
-                {
-                    if (!row.ContainsKey(field.Name)) continue;
-                    if (!IsCollectionType<IEnumerable>(field.FieldType)) continue;
-                    if (row[field.Name].IsNullOrEmpty()) continue;
-
-                    object fieldValue = row[field.Name];
-
-                    var fieldType = IsCollectionType<Array>(field.FieldType) ?
-                        field.FieldType.GetElementType() :
-                        field.FieldType.GenericTypeArguments.First();
-
-                    if (fieldType is null)
-                        throw new Exception($"Unable to get {field.Name} field type");
-
-                    fieldValue = Parse(fieldType, fieldValue);
-
-                    var getInstanceValue = field.GetValue(instances.Last());
-                    // Add value to array Type filed
-                    if (IsCollectionType<Array>(field.FieldType))
-                    {
-                        var resultList = ((IEnumerable)getInstanceValue).Cast<object>().ToList();
-                        resultList.Add(fieldValue);
-                        var resultArray = Array.CreateInstance(fieldType, resultList.Count);
-                        Array.Copy(resultList.ToArray(), resultArray, resultList.Count);
-                        field.SetValue(instances.Last(), resultArray);
-                    }
-                    // Add value to Collection type filed
-                    else
-                    {
-                        var iCollectionObject = typeof(ICollection<>).MakeGenericType(fieldType);
-                        var addMethod = iCollectionObject.GetMethod("Add");
-                        addMethod.Invoke(getInstanceValue, new object[] { fieldValue });
-                        field.SetValue(instances.Last(), getInstanceValue);
+                        var SetValueMethod = GetMethod(member, ValueMethods.SetValue);
+                        SetValueMethod.Invoke(member, new object?[] { instances.Last(), getInstanceValue });
                     }
                 }
             }
 
             return instances;
+        }
+
+        public static Type GetType(MemberInfo member)
+        {
+            return member.MemberType switch
+            {
+                MemberTypes.Property => (member as PropertyInfo).PropertyType,
+                MemberTypes.Field => (member as FieldInfo).FieldType,
+                _ => throw new Exception($"Incorrect memberInfo type. MemberInfo type: {member.MemberType}")
+            };
+        }
+
+        public enum ValueMethods
+        {
+            GetValue,
+            SetValue
+        }
+
+        public static MethodInfo GetMethod(MemberInfo member, ValueMethods method)
+        {
+            return member.MemberType switch
+            {
+                MemberTypes.Property => (member as PropertyInfo)
+                    .GetType().GetMethods().ToList().FindAll(x => x.Name.Equals(method.ToString())).Last(),
+                MemberTypes.Field => (member as FieldInfo)
+                    .GetType().GetMethods().ToList().FindAll(x => x.Name.Equals(method.ToString())).Last(),
+                _ => throw new Exception($"Incorrect memberInfo type. MemberInfo type: {member.MemberType}")
+            };
         }
 
         private static bool IsCollectionType<T>(Type propType)
